dfdcc2dd7f0fbf8b769a6a8ea5a4b20d92e38205
Tương Quỳnh Nguyễn <tuongquynhnguyen@Tuongs-MacBook-Pro.local>
2022-01-19 16:47:39 +0700

    Final of Labwork 1

---
+++ LABWORK 1 - TCP/README.md
@@ -0,0 +1,19 @@
+USTH ICT 2022 Distributed Systems
+=====================================
+
+*Groups* are expected to:
+
+* Fork this repository to your group's github account
+* Update student names and IDs to this README file
+* Push your commits regularly, with proper commit messages
+* Use pull request for collaborations!
+
+Group Info
+=======================
+
+* Nguyễn Trấn Nguyên - BI10-128
+* Mai Xuân Hiếu - BI10-064
+* Nguyễn Anh Quân - BI10-149
+* Nguyễn Tương Quỳnh - BI10-154
+* Nguyễn Quang Vinh - BI10-195
+
---
+++ LABWORK 1 - TCP/client.c
@@ -0,0 +1,52 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+int main(int argc, char* argv[]) {
+    int so;
+    char s[100];
+    struct sockaddr_in ad;
+
+    socklen_t ad_length = sizeof(ad);
+    struct hostent *hep;
+
+    // create socket
+    int serv = socket(AF_INET, SOCK_STREAM, 0);
+
+    // init address
+    hep = gethostbyname(argv[1]);
+    memset(&ad, 0, sizeof(ad));
+    ad.sin_family = AF_INET;
+    ad.sin_addr = *(struct in_addr*)hep->h_addr_list[0];
+    ad.sin_port = htons(12345);
+
+    // connect to server
+    connect(serv, (struct sockaddr*)&ad, ad_length);
+    
+    memset(&s, 0, 100);
+    FILE* file;
+    file = fopen("send_file.txt", "r");
+    if (file == NULL) {
+    	printf("The file is null");
+    } else {
+    	printf("Read file successfully\n");
+    }
+    
+    char buffer[1024] = {0};
+    while (fgets(buffer, sizeof(buffer), file) != NULL) {
+    	int i = send(serv, buffer, sizeof(buffer), 0);
+    	if (i == -1) {
+    		printf("Send data fail");
+    	}
+    	memset(&buffer, 0, sizeof(buffer));
+    }
+    printf("File sent!");
+    
+    close(serv);
+    return 0;
+}
+
